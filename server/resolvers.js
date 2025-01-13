e9310bca9765a17b9acc26321ddb83f7d7a92acd
JesusGtz94 <jesusgtz.dev@gmail.com>
2025-01-12 23:40:44 -0500

    job and company schemas

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,17 @@
+import { getCompany } from "./db/companies.js";
 import { getJobs } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
     jobs: getJobs,
+  },
+
+  Job: {
+    date: ({ createdAt }) => createdAt.split("T")[0],
+    company: ({ companyId }) => getCompany(companyId),
+  },
+
+  Company: {
+    desc: ({ description }) => description,
   },
 };
