72a9163e9f0059d7a45438bbc16e088445e16517
JesusGtz94 <jesusgtz.dev@gmail.com>
2025-01-13 17:09:28 -0500

    fetch jobs from graphql

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,23 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import { useEffect, useState } from "react";
+import JobList from "../components/JobList";
+import { getJobs } from "../lib/grapql/queries";
 
 function HomePage() {
+  const [jobs, setJobs] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    getJobs()
+      .then(setJobs)
+      .catch(() => setError("An error occurred."));
+  }, []);
+
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
-      <JobList jobs={jobs} />
+      <h1 className="title">Job Board</h1>
+      {!jobs && !error && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {jobs && <JobList jobs={jobs} />}
     </div>
   );
 }
